3500ce82582b28cb44185f6cfaaa23a67eb4bc9c
Mathis Tessier <mtessier809@gmail.com>
2023-06-03 17:21:49 -0700

    changed descriptions and title of webpage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,17 +9,17 @@ const Home = () => {
     >
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">Hi, my name is</p>
+        <p className="text-pink-600 text-xl">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Mathis Tessier
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm an aspiring Software Engineer.
+          I'm a Junior Software Engineer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I'm a recent college graduate looking to build proffesional experience
+          I'm a recent college graduate looking to build professional experience
           in the tech industry. Currently, I work on building responsive
-          full-stack web applications using React and Amazon Web Services.
+          full-stack web applications using React.js and Amazon Web Services.
         </p>
         <div>
           <Link to="work" smooth={true} duration={500}>
