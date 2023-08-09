e7e4b0cc7cf29035e60a223a26f128c01b62c4f6
Mathis Tessier <mtessier809@gmail.com>
2023-08-08 19:53:12 -0700

    linked updated portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -146,7 +146,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1Cdf2IqnqIJ_JzmAf8XNy_UPzRf7LErQf/view?usp=sharing"
+              href="https://drive.google.com/file/d/1XWxJ9zj-Q4LkX-FOYi_PbaX7mm8edXDu/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
